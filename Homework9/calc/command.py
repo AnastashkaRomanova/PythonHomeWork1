c17fa8def4df4cfb5f93bd380710ad32de441b6f
Anastasiya <piterskayaz@mail.ru>
2022-11-13 23:43:11 +0300

    calc

---
+++ Homework9/calc/command.py
@@ -0,0 +1,23 @@
+def comp(a, b):
+    return complex(a, b)
+
+def div(a, b):
+    return a / b
+
+def int_div(a, b):
+    return a // b
+
+def mult(a, b):
+    return a * b
+
+def power(a, b):
+    return a ** b
+
+def rem_of_div(a, b):
+    return a % b
+
+def sub(a, b):
+    return a - b
+
+def sum(a, b):
+    return a + b            
